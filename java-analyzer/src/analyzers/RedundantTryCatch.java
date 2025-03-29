2f96da85b397f58b4b05d6d2d6d84513d797c460
softpoint-arch <46046667+softpoint-arch@users.noreply.github.com>
2025-03-28 20:40:59 -0700

    Implemented RedundantCatch class and test

--- java-analyzer/src/analyzers/RedundantTryCatch.java
+++ java-analyzer/src/analyzers/RedundantTryCatch.java
@@ -1,6 +1,41 @@
 package analyzers;
 
-public class RedundantTryCatch {
-    // Redundant try-catch blocks (e.g. we can use Files.exists() to check file existence rather than catching FileNotFoundException)
+import com.github.javaparser.ast.stmt.CatchClause;
+import model.AnalysisResult;
+import model.RiskLevel;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class RedundantTryCatch extends BaseAnalyzer {
+    // Redundant try-catch blocks (e.g. we can use Files.exists() to check file existence rather than catching FileNotFoundException)
+    public static final String ERROR_CODE = "REDUNDANT_TRY_CATCH";
+    public static final String ERROR_MESSAGE = "Redundant try-catch block for exception: ";
+    public static final String SUGGESTION_PART1 = "Consider checking ";
+    public static final String SUGGESTION_PART2 = " instead to avoid unnecessary try-catch blocks for ";
+    public static final RiskLevel RISK_LEVEL = RiskLevel.LOW;
+
+    private static final Set<String> REDUNDANT_EXCEPTIONS = Set.of(
+            "FileNotFoundException", "NullPointerException", "IndexOutOfBoundsException",
+            "ArithmeticException", "NoSuchElementException"
+    );
+
+    private static final Map<String, String> POSSIBLE_FIX = Map.of(
+            "FileNotFoundException", "File.exists()",
+            "NullPointerException", "Objects.nonNull(obj)",
+            "IndexOutOfBoundsException", "list.size() > index",
+            "ArithmeticException", "<denominator> != 0",
+            "NoSuchElementException", "Scanner.hasNext()"
+    );
+
+    @Override
+    public void visit(CatchClause catchClause, List<AnalysisResult> results) {
+        super.visit(catchClause, results);
+        String exceptionName = catchClause.getParameter().getType().asString();
+        if (REDUNDANT_EXCEPTIONS.contains(exceptionName)) {
+            String suggestion = SUGGESTION_PART1 + POSSIBLE_FIX.get(exceptionName) + SUGGESTION_PART2 + exceptionName;
+            addAnalysisResult(results, ERROR_CODE, catchClause, ERROR_MESSAGE + exceptionName, suggestion, RISK_LEVEL);
+        }
+    }
 }
