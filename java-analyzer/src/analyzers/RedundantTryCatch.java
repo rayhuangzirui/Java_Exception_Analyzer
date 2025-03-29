f8d6f3e7d00b053c36992e43939e9bfa507616fc
softpoint-arch <46046667+softpoint-arch@users.noreply.github.com>
2025-03-28 18:12:56 -0700

    Added test for redundant catch

--- java-analyzer/src/analyzers/RedundantTryCatch.java
+++ java-analyzer/src/analyzers/RedundantTryCatch.java
@@ -2,4 +2,5 @@ package analyzers;
 
 public class RedundantTryCatch {
     // Redundant try-catch blocks (e.g. we can use Files.exists() to check file existence rather than catching FileNotFoundException)
+
 }
