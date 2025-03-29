39ec72516bad79570b7eea12307804f274e984fc
calvinzz <calvinzz@student.ubc.ca>
2025-03-28 23:22:50 -0700

    Test for AlwaysTriggeredCatch

---
+++ java-analyzer/test/analyzersTests/AlwaysTriggeredCatchTest.java
@@ -0,0 +1,57 @@
+package analyzersTests;
+
+import analyzers.AlwaysTriggeredCatch;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.stmt.CatchClause;
+import model.AnalysisResult;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class AlwaysTriggeredCatchTest {
+    @Test
+    public void detectsFileNotFound() {
+        // Get errorCode from resources/sample-java/EmptyCatchExample.java by replacing the path
+        String path = "../resources/sample-java/AlwaysTriggeredCatchExample1.java";
+        // Get the errorCode from the file
+        try {
+            String code = Files.readString(Path.of(path));
+            System.out.println("Code: " + code);
+            CompilationUnit cu = StaticJavaParser.parse(code);
+            CatchClause cc = getFirstCatchClause(cu);
+            System.out.println("Issue: " + cc);
+            List<AnalysisResult> results = analyze(cu);
+
+            assertEquals(1, results.size());
+            assertEquals(AlwaysTriggeredCatch.ERROR_CODE, results.getFirst().errorCode());
+            assertEquals(18, results.getFirst().startLine());
+            assertEquals(AlwaysTriggeredCatch.ERROR_MESSAGE, results.getFirst().message());
+            assertEquals(cc, results.getFirst().node());
+            assertEquals(AlwaysTriggeredCatch.SUGGESTION, results.getFirst().suggestion());
+            assertEquals("HIGH", results.getFirst().riskLevel().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private CatchClause getFirstCatchClause(CompilationUnit cu) {
+
+        Optional<CatchClause> catchClause = cu.findFirst(CatchClause.class);
+        return catchClause.orElseThrow();
+    }
+
+    private List<AnalysisResult> analyze(CompilationUnit cu) {
+        AlwaysTriggeredCatch analyzer = new AlwaysTriggeredCatch();
+        List<AnalysisResult> results = new ArrayList<>();
+        analyzer.visit(cu, results);
+        return results;
+    }
+}
